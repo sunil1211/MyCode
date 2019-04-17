61a5094be89f7df8bda6562256799b3e8a90f486
sunil1211 <42082661+sunil1211@users.noreply.github.com>
2019-04-17 18:43:28 +0530

    city care hospital
    a simple web design by me

---
+++ navbar.js
@@ -0,0 +1,15 @@
+
+   function myFunction()
+            {
+                var x = document.getElementsById("myTopnav");
+                {
+                    if(x.className === "topnav")
+                    {
+                        x.className += " responsive";
+                    }
+                    else
+                    {
+                        x.className = "topnav";
+                    }
+                }
+            }
